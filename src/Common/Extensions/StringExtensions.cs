e48a6ed7624e43ab463599ae9599d0ae32ab93e1
bmoyer240 <bmoyer240@users.noreply.github.com>
2023-06-23 10:58:07 -0400

    task: updated StringExtensions with code analysis attribute

--- src/Common/Extensions/StringExtensions.cs
+++ src/Common/Extensions/StringExtensions.cs
@@ -1,21 +1,23 @@
-﻿namespace System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
 
-internal static class StringExtensions
+namespace Application;
+
+public static class StringExtensions
 {
-        /// <summary>
-        ///     Simple wrapper for <c>IsNullOrWhiteSpace</c>
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static bool IsEmpty(this string? input) =>
-            string.IsNullOrWhiteSpace(input);
+    /// <summary>
+    ///     Simple wrapper for <c>IsNullOrWhiteSpace</c>
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static bool IsEmpty([NotNullWhen(false)] this string? input) =>
+        string.IsNullOrWhiteSpace(input);
 
-        /// <summary>
-        ///     Simple wrapper for <c>IsNullOrWhiteSpace</c> with a bang!
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static bool IsNotEmpty(this string? input) =>
-            !input.IsEmpty();
+    /// <summary>
+    ///     Simple wrapper for <c>IsNullOrWhiteSpace</c> with a bang!
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static bool IsNotEmpty([NotNullWhen(true)] this string? input) =>
+        string.IsNullOrWhiteSpace(input) == false;
 
 }
